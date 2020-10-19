93ac888f0e04abb7b8e27cde2b24a852401521b3
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-19 02:14:09 -0700

    initial Landing & Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,14 @@
 import React, { Fragment } from 'react';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
 import './App.css';
-
-import SignUp from './components/SignUp';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Jumbotron from 'react-bootstrap/Jumbotron'
-
-import bgimage from './assets/img/bgImg@x2.png';
 
 function App() {
   return (
-   <Jumbotron style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover', width: '100vw', height: '100vh' }}>
-      <Container className="App" style={{alignContent: "center"}}>
-        {/* <Header
-          navItems={navItems}
-          setCurrentNavItem={setCurrentNavItem}
-          currentNavItem={currentNavItem}
-        ></Header> */}
-
-        <Row>
-          <Col>
-            <SignUp></SignUp>
-          </Col>
-        </Row>
-{/* 
-        <Row>
-          <Footer></Footer>
-        </Row> */}
-
-      </Container>
-    </Jumbotron>
+    <Fragment>
+      <Navbar />
+      <Landing />
+    </Fragment>
   );
 }
 
---
+++ client/src/components/layout/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <a href="index.html">
+          <i className="fas fa-compact-disc"></i> Record Crate
+        </a>
+      </h1>
+      <ul>
+        <li>
+          <a href="register.html">Register</a>
+        </li>
+        <li>
+          <a href="login.html">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
