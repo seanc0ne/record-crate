3fd00eb89fe032e040a8a33a4a187ebfaeca1dd2
jaaybe <jb@motionmusic.us>
2020-10-18 17:06:04 -0700

    initial assets and landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,37 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import SignUp from './components/SignUp';
+
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+
+import bgimage from './assets/img/bgImg@x2.png';
+
 function App() {
   return (
-    <Fragment>
-      <h1>App</h1>
-    </Fragment>
+   <Jumbotron style={{ backgroundImage: `url(${bgimage})`, backgroundSize: 'cover', width: '100vw', height: '100vh' }}>
+      <Container className="App" style={{alignContent: "center"}}>
+        {/* <Header
+          navItems={navItems}
+          setCurrentNavItem={setCurrentNavItem}
+          currentNavItem={currentNavItem}
+        ></Header> */}
+
+        <Row>
+          <Col>
+            <SignUp></SignUp>
+          </Col>
+        </Row>
+{/* 
+        <Row>
+          <Footer></Footer>
+        </Row> */}
+
+      </Container>
+    </Jumbotron>
   );
 }
 
---
+++ client/src/components/LandingSleeve/index.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from 'react';
+
+import Image from 'react-bootstrap/Image';
+import Button from 'react-bootstrap/Button';
+
+import recordSleeveLogo from '../../assets/img/recordSleeveWtLogo@x2.png'
+
+
+function SignUp() {
+    return (
+        <Image src={recordSleeveLogo} fluid />
+    );
+
+} 
+
+export default SignUp;
