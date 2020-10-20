4b7e78926368c52bd75121a0e3c93a2ab7574e19
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-20 05:18:50 -0700

    Protect Route for Dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,8 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
 // Redux
@@ -32,6 +34,7 @@ function App() {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
