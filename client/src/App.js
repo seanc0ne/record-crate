9fbecdb71c87a24532eedb694face96c9578139d
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-22 07:37:28 -0700

    landing page with signup button merged

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,29 @@
+// *********** REACT, REDUX & UTILS **********
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { loadUser } from './actions/auth';
+import store from './store';
+import setAuthToken from './utils/setAuthToken';
+
+// *********** COMPONENTS **********
 import Landing from './components/layout/Landing';
+// import LandingSleeve from './components/LandingSleeve';
 import Register from './components/auth/Register';
+// import SignUp from './components/auth/SignUp';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import Dashboard from './components/dashboard/Dashboard';
 import EditUser from './components/auth/EditUser';
 import Tracks from './components/tracks/Tracks';
 import PrivateRoute from './components/routing/PrivateRoute';
-import { loadUser } from './actions/auth';
-import setAuthToken from './utils/setAuthToken';
-// Redux
-import { Provider } from 'react-redux';
-import store from './store';
-// custom styles
+
+// *********** BOOTSTRAP & CUSTOM STYLES **********
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 import './App.css';
 
 // check localStorage for a token and set the global headers with it if there is one
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -1,7 +1,17 @@
+// *********** REACT & REDUX **********
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+
+// *********** BOOTSTRAP & ASSETS **********
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+import recordSleeveLogo from '../../assets/img/recordSleeveWtLogo@x2.png';
 
 const Landing = ({ isAuthenticated }) => {
   if (isAuthenticated) {
@@ -9,20 +19,27 @@ const Landing = ({ isAuthenticated }) => {
   }
   return (
     <section className="landing">
-      <div className="dark-overlay">
-        <div className="landing-inner">
-          <h1 className="x-large">Record Crate</h1>
-          <p className="lead">Blurb about record crate</p>
-          <div className="buttons">
-            <Link to="/register" className="btn btn-primary">
-              Sign Up
-            </Link>
-            <Link to="/login" className="btn btn-light">
-              Login
-            </Link>
-          </div>
-        </div>
-      </div>
+      <Container className="pt-5" fluid>
+        <Row className="mx-auto vw-75 vh-100">
+          <Col className="my-auto">
+            <Card className="landingCard text-white">
+              <Card.Img src={recordSleeveLogo} />
+              <Card.ImgOverlay>
+                <Card.Text className="landingText">
+                  Create your own playlist from our database full of all the
+                  details you’ve been missing including BPM, key, and much
+                  more...
+                </Card.Text>
+                <div className="landingBtnDiv mt-3">
+                  <Link to="/register" className="btn btn-primary landingBtn">
+                    Sign Up
+                  </Link>
+                </div>
+              </Card.ImgOverlay>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </section>
   );
 };
