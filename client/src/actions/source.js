d3310bcd18772e8b0299c92ef39d56f4e8ff059e
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-26 22:26:36 -0700

    track-crud set-up

--- client/src/actions/source.js
+++ client/src/actions/source.js
@@ -1,26 +1,53 @@
-// Add a source
-// export const addSource = (formData) => async (dispatch) => {
-//   try {
-//     const config = {
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//     };
-//     const res = await axios.put('/api/source', formData, config);
-//     dispatch({
-//       type: UPDATE_TRACK,
-//       payload: res.data,
-//     });
-//     dispatch(setAlert('Source added', 'success'));
-//   } catch (err) {
-//     const errors = err.response.data.errors; // we want to display the array of errors
-//     // if there are errors we want to dispatch an alert for each of them
-//     if (errors) {
-//       errors.forEach((error) => dispatch(setAlert(error.msg, 'danger')));
-//     }
-//     dispatch({
-//       type: TRACK_ERROR,
-//       payload: { msg: err.response.statusText, status: err.response.status },
-//     });
-//   }
-// };
+import axios from 'axios';
+import { setAlert } from './alert';
+import {
+  GET_SOURCE,
+  GET_SOURCES,
+  CLEAR_SOURCE,
+  SOURCE_ERROR,
+  DELETE_SOURCE,
+  ADD_SOURCE,
+} from './types';
+
+// Get all sources
+export const getSources = () => async (dispatch) => {
+  // dispatch({ type: CLEAR_SOURCE });
+  try {
+    const res = await axios.get('/api/source');
+    dispatch({
+      type: GET_SOURCES,
+      payload: res.data,
+    });
+  } catch (err) {
+    dispatch({
+      type: SOURCE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add source
+export const addSource = (formData) => async (dispatch) => {
+  console.log('formData from within addSource action: ', formData);
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    console.log('config', config);
+    console.log('formData', formData);
+    const res = await axios.post('/api/source', formData, config);
+    dispatch({
+      type: ADD_SOURCE,
+      payload: res.data,
+    });
+    console.log('res.data', res.data);
+    dispatch(setAlert('Source added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: SOURCE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
