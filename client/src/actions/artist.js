d1a0508e7c941c61b02f2361d810fb2584c2a266
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-10-26 22:24:46 -0700

    track-crud set-up

--- client/src/actions/artist.js
+++ client/src/actions/artist.js
@@ -6,6 +6,7 @@ import {
   UPDATE_ARTIST,
   CLEAR_ARTIST,
   ARTIST_ERROR,
+  ADD_ARTIST,
 } from './types';
 
 // Get all artists
@@ -17,6 +18,28 @@ export const getArtists = () => async (dispatch) => {
       type: GET_ARTISTS,
       payload: res.data,
     });
+  } catch (err) {
+    dispatch({
+      type: ARTIST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add artist
+export const addArtist = (formData) => async (dispatch) => {
+  const config = {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  try {
+    const res = await axios.post('/api/artist', formData, config);
+    dispatch({
+      type: ADD_ARTIST,
+      payload: res.data,
+    });
+    dispatch(setAlert('Artist added', 'success'));
   } catch (err) {
     dispatch({
       type: ARTIST_ERROR,
